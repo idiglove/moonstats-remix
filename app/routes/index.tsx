9071bfd24c581d8f9c1cd6bb8395ca2908db4cd6
Faith Morante <faith.morante@gmail.com>
2022-06-06 12:28:12 +0800

    watch css files

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
       className="dashboard"
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
     >
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl text-white">Welcome to Remix</h1>
       <ul>
         <li>
           <a
